a872f46d56afac63b5deb34468fddc9fe1f7f06d
forcheng <2871979271@qq.com>
2020-05-27 17:27:36 +0800

    forcheng - use material components

--- lib/main.dart
+++ lib/main.dart
@@ -2,57 +2,28 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(new MaterialApp(
-    title: 'My app',
-    home: new MyScaffold(),
+    title: 'Flutter Tutorial',
+    home: new TutorialHome(),
   ));
 }
 
-class MyAppBar extends StatelessWidget {
-  MyAppBar({ this.title });
-
-  final Widget title;
-
+class TutorialHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new Container(
-      height: 56.0,
-      padding: const EdgeInsets.symmetric(horizontal: 8.0),
-      decoration: new BoxDecoration(color: Colors.blue[500]),
-      child: new Row(
-        children: <Widget>[
-          new IconButton(
-            icon: new Icon(Icons.menu),
-            tooltip: 'Navigation menu',
-            onPressed: null,
-          ),
-          new Expanded(child: this.title),
-          new IconButton(
-            icon: new Icon(Icons.search),
-            tooltip: 'Search',
-            onPressed: null
-          )
+    return new Scaffold(
+      appBar: new AppBar(
+        leading: new IconButton(icon: new Icon(Icons.menu), tooltip: 'Navigation menu', onPressed: null),
+        title: new Text('Example Title'),
+        actions: <Widget>[
+          new IconButton(icon: new Icon(Icons.search), tooltip: 'Search', onPressed: null)
         ],
       ),
+      body: new Center(child: new Text('Hello world')),
+      floatingActionButton: new FloatingActionButton(
+        tooltip: 'Add',
+        child: new Icon(Icons.add),
+        onPressed: null
+      ),      
     );
   }
-}
-
-class MyScaffold extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return new Material(
-      child: new Column(
-        children: <Widget>[
-          new MyAppBar(
-            title: new Text(
-              'Example title',
-              style: Theme.of(context).primaryTextTheme.title,
-            )
-          ),
-          new Expanded(child: new Center(child: new Text('Hello world')))
-        ]
-      )
-    );
-  }
-
 }
