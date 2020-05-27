cff5ea5c7e17eb5bb11079f325a26f9bcedfc758
forcheng <2871979271@qq.com>
2020-05-27 17:28:53 +0800

    forcheng - use GestureDetector for MyButton

--- lib/main.dart
+++ lib/main.dart
@@ -18,12 +18,35 @@ class TutorialHome extends StatelessWidget {
           new IconButton(icon: new Icon(Icons.search), tooltip: 'Search', onPressed: null)
         ],
       ),
-      body: new Center(child: new Text('Hello world')),
+      body: new MyButton(),
       floatingActionButton: new FloatingActionButton(
         tooltip: 'Add',
         child: new Icon(Icons.add),
         onPressed: null
       ),      
+    );
+  }
+}
+
+class MyButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new GestureDetector(
+      onTap: () {
+        print('MyButton was tapped!');
+      },
+      child: new Container(
+        height: 36.0,
+        padding: const EdgeInsets.all(8.0),
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        decoration: new BoxDecoration(
+          borderRadius: new BorderRadius.circular(5.0),
+          color: Colors.lightGreen[500],
+        ),
+        child: new Center(
+          child: new Text('Engage'),
+        ),
+      ),
     );
   }
 }
