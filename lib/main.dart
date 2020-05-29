e118e8bb56305dd56b95e528ea5af4078f8c0ab5
forcheng <2871979271@qq.com>
2020-05-29 15:46:14 +0800

    forcheng - show different types of items for list

--- lib/main.dart
+++ lib/main.dart
@@ -3,18 +3,23 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(new MyApp(
-    items: new List<String>.generate(10000, (i) => "Item $i"),
+    items: new List<ListItem>.generate(
+      1000,
+      (i) => i % 6 == 0
+          ? new HeadingItem("Heading $i")
+          : new MessageItem("Sender $i", "Message body $i"),
+    ),
   ));
 }
 
 class MyApp extends StatelessWidget {
-  final List<String> items;
+  final List<ListItem> items;
 
   MyApp({Key key, @required this.items}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final title = 'Long List';
+    final title = 'Mixed List';
 
     return new MaterialApp(
       title: title,
@@ -25,12 +30,39 @@ class MyApp extends StatelessWidget {
         body: new ListView.builder(
           itemCount: items.length,
           itemBuilder: (context, index) {
-            return new ListTile(
-              title: new Text('${items[index]}'),
-            );
+            final item = items[index];
+
+            if (item is HeadingItem) {
+              return new ListTile(
+                title: new Text(
+                  item.heading,
+                  style: Theme.of(context).textTheme.headline,
+                ),
+              );
+            } else if (item is MessageItem) {
+              return new ListTile(
+                title: new Text(item.sender),
+                subtitle: new Text(item.body),
+              );
+            }
           },
         ),
       ),
     );
   }
+}
+
+abstract class ListItem {}
+
+class HeadingItem implements ListItem {
+  final String heading;
+
+  HeadingItem(this.heading);
+}
+
+class MessageItem implements ListItem {
+  final String sender;
+  final String body;
+
+  MessageItem(this.sender, this.body);
 }
